eaca2790bbcfc52db0442c2c21b3a2a7566fed1a
NateOgden <Nathanogden@mail.weber.edu>
2017-02-23 00:47:08 -0700

    test

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>I'm Up and Running!</h1>
+          <h1>I'm Up and Running Still!</h1>
       </div>
     );
   }
 }
 
-export default App;
+export default App; 
